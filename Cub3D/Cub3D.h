791103dec85d4d40e8450f0e3b71d92ecd3253a7
vdauverg <vdauverg@e1z2r6p5.42.us.org>
2020-09-14 13:37:42 -0700

    fixed option reading

--- Cub3D/Cub3D.h
+++ Cub3D/Cub3D.h
@@ -6,14 +6,15 @@
 /*   By: vdauverg <vdauverg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/26 17:48:05 by vdauverg          #+#    #+#             */
-/*   Updated: 2020/09/09 12:30:37 by vdauverg         ###   ########.fr       */
+/*   Updated: 2020/09/14 13:30:05 by vdauverg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D
-# define CUB3D
+#ifndef CUB3D_H
+# define CUB3D_H
 # include "./minilibx_opengl_20191021/mlx.h"
-# include "./Libft/libft.h"
+# include "./Libft/inc/libft.h"
+# include "./Libft/inc/get_next_line.h"
 # include <fcntl.h>
 # include <stdio.h>
 # include <math.h>
@@ -22,8 +23,8 @@
 # define USAGE 1
 # define INC_FORM 2
 # define INC_FLAG 3
-# define INC_READ 3
-# define INC_TXID 4
+# define INC_READ 4
+# define INC_TXID 5
 
 typedef struct	s_options
 {
--- Cub3D/src/Cub3D.c
+++ Cub3D/src/Cub3D.c
@@ -6,7 +6,7 @@
 /*   By: vdauverg <vdauverg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/26 17:47:29 by vdauverg          #+#    #+#             */
-/*   Updated: 2020/09/09 13:12:31 by vdauverg         ###   ########.fr       */
+/*   Updated: 2020/09/14 13:36:30 by vdauverg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,35 +40,6 @@ void	safe_exit(int e)
 	exit(1);
 }
 
-char	*read_line(int fd, char **line)
-{
-	int		n;
-	char	*buf;
-	char	**split;
-
-	n = -1;
-	while (n && !ft_strchr(*line, '\n') && (buf = ft_strnew(64)))
-	{
-		if (!(n = read(fd, buf, 64)) && *line == NULL)
-			return (NULL);
-		if (n == -1)
-			safe_exit(INC_READ);
-		*line = ft_strjoinfreefirst(*line, buf);
-		ft_strdel(&buf);
-	}
-	if (!n && (buf = ft_strdup(*line)))
-		ft_strdel(line);
-	else
-	{
-		split = ft_strsplit(*line, '\n');
-		buf = ft_strjoinfreefirst(split[0], "");
-		ft_strdel(line);
-		*line = ft_strjoinfreefirst(split[1], "");
-		ft_memdel((void **)&split);
-	}
-	return (buf);
-}
-
 int		check_filepath(char *file)
 {
 	int		fd;
@@ -84,26 +55,28 @@ int		check_filepath(char *file)
 	return (fd);
 }
 
-void	check_texture(char *id, char *line)
-{
-	int	l;
-	int n;
+// void	check_type_id(char* id, char *line)
+// {
+// 	;
+// }
 
-	l = ft_strlen(id);
-	if (strncmp(id, line, l))
-		safe_exit(INC_TXID);
+// void	check_texture(char *id, char *line)
+// {
+// 	int	l;
+// 	int n;
 
-	n = ft_atoi(line + 1);
+// 	l = ft_strlen(id);
+// 	if (strncmp(id, line, l))
+// 		safe_exit(INC_TXID);
 
-}
+// 	n = ft_atoi(line + 1);
+// }
 
 void	check_meta(int fd, t_meta *meta)
 {
 	char	*line;
-	char	*tmp;
 
-	tmp = ft_strdup("");
-	while ((line = read_line(fd, &tmp)))
+	while ((get_next_line(fd, &line)) == 1)
 		ft_outputstr(1, line);
 
 	meta = NULL;
@@ -139,12 +112,14 @@ void	check_flag(char *flag, t_options *opts)
 	if (!flag)
 		safe_exit(INC_FLAG);
 
-	if (ft_strcmp("--", flag) != 2)
+	if (ft_strnequ("--", flag, 2))
 		safe_exit(INC_FLAG);
 
 	flag += 2;
 	if (ft_strequ("save", flag))
 		opts->save = 1;
+	else
+		safe_exit(INC_FLAG);
 }
 
 void	check_format(char *file)
